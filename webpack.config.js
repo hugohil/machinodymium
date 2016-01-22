58bfbc596f76683422f3374e25de4e8ea89fcaa4
hugohil <hugo@soixantecircuits.fr>
2016-01-22 17:23:20 +0100

    "initial commit"

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: './app/entry.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: 'http://0.0.0.0:6060/',
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {test: /\.html/, loader: 'html-loader'},
+      {test: /\.css$/, loader: 'style-loader!css-loader'},
+      {test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/, loader: 'file-loader'},
+      {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'},
+      {test: /\.mp4$/, loader: 'file-loader'}
+    ]
+  },
+  
+}
